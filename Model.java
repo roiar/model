a852eb98a0437782650190af383852de5441ac91
thanasis <roiarapoglou@gmail.com>
2018-01-23 09:51:39 +0000

    add another argument

--- Model.java
+++ Model.java
@@ -1,7 +1,8 @@
 public class Model {
 
-    public view(String message) {
+    public view(String message, int number) {
         System.out.println("The incoming message is " + message);
+	System.out.println("The incoming number is " + number);
     }
 }
 
